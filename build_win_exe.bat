373da5c77b2d7c666921f632b14cc4747642338e
jacob-jarick <mem.namefix@gmail.com>
2017-08-21 14:57:59 +1000

    -M HTTP::Server::Simple::CGI

--- build_win_exe.bat
+++ build_win_exe.bat
@@ -1,3 +1,3 @@
 echo CLI
 del rmplayer.exe
-cmd /c pp -u -M Proc::Background -M File::Spec::Functions -o rmplayer.exe rmplayer.pl
+cmd /c pp -u -M Proc::Background -M File::Spec::Functions -M HTTP::Server::Simple::CGI -o rmplayer.exe rmplayer.pl
