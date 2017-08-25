1ff6962aabfafd69c837bdb63af62ccc09ae0307
jacob-jarick <mem.namefix@gmail.com>
2017-08-25 21:56:50 +1000

    pp build options tweaked

--- build_win_exe.bat
+++ build_win_exe.bat
@@ -12,16 +12,21 @@ if "%HOUR:~0,1%" == " " (SET dtStamp=%dtStamp9%) else (SET dtStamp=%dtStamp24%)
 echo SET Build Date
 ECHO %dtStamp% > builddate.txt
 
-echo Build rmplayer.exe
-del rmplayer.exe
-cmd /c pp -u -M File::Spec::Functions -M HTTP::Server::Simple::CGI -M CGI::Carp -M List::Util -M Term::ReadKey -M Config::IniHash -M Scalar::Util -M JSON -M File::stat -M threads -M threads::shared -M CGI -M List::MoreUtils -o rmplayer.exe rmplayer.pl
+echo Delete *.exe files
 
-echo Build config.exe
+del rmplayer.exe
 del config.exe
-cmd /c pp -u -M Tk -M Tk::ROText -M Tk::Spinbox -M File::Spec::Functions -M Config::IniHash -M JSON -M List::MoreUtils -M Tk::NoteBook -M Tk::Chart::Pie -M Tk::Graph -o config.exe config.pl
+del update.exe
+
+echo Build rmplayer.exe
+cmd /c pp -z 9 -u -M File::Spec::Functions -M HTTP::Server::Simple::CGI -M CGI::Carp -M List::Util -M Term::ReadKey -M Config::IniHash -M Scalar::Util -M JSON -M File::stat -M threads -M threads::shared -M CGI -M List::MoreUtils -o rmplayer.exe rmplayer.pl
+echo UPX rmplayer.exe
+
+cmd /c pp -z 9 -u -M Tk -M Tk::ROText -M Tk::Spinbox -M File::Spec::Functions -M Config::IniHash -M JSON -M List::MoreUtils -M Tk::NoteBook -M Tk::Chart::Pie -M Tk::Graph -o config.exe config.pl
+echo UPX config.exe
 
 echo Build update.exe
-del update.exe
-cmd /c pp -u -M LWP::Simple -M LWP::UserAgent -M File::Fetch -M Archive::Zip -o update.exe update.pl
+cmd /c pp -z 9 -u -M LWP::Simple -M LWP::UserAgent -M File::Fetch -M Archive::Zip -o update.exe update.pl
+echo UPX update.exe
 
 echo Done
