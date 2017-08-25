5cdec81de4297eb4193a8d3c4cc5a71c5620c559
jacob-jarick <mem.namefix@gmail.com>
2017-08-25 21:05:47 +1000

    auto update - compare build date of exes

--- build_win_exe.bat
+++ build_win_exe.bat
@@ -1,8 +1,27 @@
-echo CLI
+@ECHO OFF
+: Sets the proper date and time stamp with 24Hr Time for log file naming
+: convention
+
+REM date string borrowed from: https://stackoverflow.com/questions/1192476/format-date-and-time-in-a-windows-batch-script
+SET HOUR=%time:~0,2%
+SET dtStamp9=%date:~-4%%date:~4,2%%date:~7,2%_0%time:~1,1%%time:~3,2%%time:~6,2%
+SET dtStamp24=%date:~-4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%
+
+if "%HOUR:~0,1%" == " " (SET dtStamp=%dtStamp9%) else (SET dtStamp=%dtStamp24%)
+
+echo SET Build Date
+ECHO %dtStamp% > builddate.txt
+
+echo Build rmplayer.exe
 del rmplayer.exe
 cmd /c pp -u -M File::Spec::Functions -M HTTP::Server::Simple::CGI -M CGI::Carp -M List::Util -M Term::ReadKey -M Config::IniHash -M Scalar::Util -M JSON -M File::stat -M threads -M threads::shared -M CGI -M List::MoreUtils -o rmplayer.exe rmplayer.pl
 
-
+echo Build config.exe
+del config.exe
 cmd /c pp -u -M Tk -M Tk::ROText -M Tk::Spinbox -M File::Spec::Functions -M Config::IniHash -M JSON -M List::MoreUtils -M Tk::NoteBook -M Tk::Chart::Pie -M Tk::Graph -o config.exe config.pl
 
+echo Build update.exe
+del update.exe
 cmd /c pp -u -M LWP::Simple -M LWP::UserAgent -M File::Fetch -M Archive::Zip -o update.exe update.pl
+
+echo Done
--- update.pl
+++ update.pl
@@ -7,17 +7,15 @@ use FindBin qw/$Bin/;
 
 use LWP::Simple qw(getstore);
 use LWP::UserAgent;
-use Digest::MD5    qw( md5_hex );
-use Digest::MD5::File qw( file_md5_hex );
 use File::Fetch;
 use Archive::Zip;
 
-my $dir		= "$Bin/updates";
-my $url		= 'https://raw.githubusercontent.com/jacob-jarick/rmplayer/master/lib/rmvars.pm';
-my $file	= &get($url);
-my $old_file	= "$Bin/lib/rmvars.pm";
-
 print "\n\nChecking for Updates\n\n";
+
+my $dir		= "$Bin/updates";
+my $url		= 'https://raw.githubusercontent.com/jacob-jarick/rmplayer/master/builddate.txt';
+my $file	= &get($url);
+my $old_file	= "$Bin/builddate.txt";
 
 if(!-d $dir)
 {
@@ -52,32 +50,25 @@ exit;
 
 sub get
 {
-	my $filename = $url;
-	$filename =~ m/.*\/(.*)$/;
-	$filename = $1;
-
-	my $save = "$dir/$filename";
-
-
-	my $ua = LWP::UserAgent->new();
-	$ua->show_progress(1);
-
-	my $response = $ua->get($url);
-	die $response->status_line if !$response->is_success;
-	my $file = $response->decoded_content( charset => 'none' );
+	my $filename	= $url;
+	$filename	=~ m/.*\/(.*)$/;
+	$filename	= $1;
+	my $save	= "$dir/$filename";
+	my $ua		= LWP::UserAgent->new();			$ua->show_progress(1);
+	my $response	= $ua->get($url);				die $response->status_line if !$response->is_success;
+	my $file	= $response->decoded_content(charset=>'none');
 
 	unlink $save;
 	getstore($url,$save);
 
 	return $save;
-
 }
 
 sub check_for_update
 {
 	print "Comparing:\n\t$old_file\n\tTO\n\t$file\n";
 
-	return 1 if !-f $old_file;
+	return if !-f $old_file;
 
 	open(FILE, $file) or die "$!";
 	my @tmp = <FILE>;
