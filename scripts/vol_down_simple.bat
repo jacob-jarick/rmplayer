ed364dcce51316eb904d60f9bac5ec01035498a7
jacob-jarick <mem.namefix@gmail.com>
2025-09-24 02:15:45 +0800

    ignore button added to webui, vol commands updated

--- scripts/vol_down_simple.bat
+++ scripts/vol_down_simple.bat
@@ -1,3 +1,3 @@
 @echo off
 REM Windows volume down using PowerShell
-powershell -Command "$obj = New-Object -com WScript.Shell; for ($i = 0; $i -lt 3; $i++) { $obj.SendKeys([char]174); Start-Sleep -Milliseconds 50 }; Write-Host 'Volume decreased'"
+start /min "" powershell -Command "$obj = New-Object -com WScript.Shell; for ($i = 0; $i -lt 3; $i++) { $obj.SendKeys([char]174); Start-Sleep -Milliseconds 50 }; Write-Host 'Volume decreased'"
--- scripts/vol_up_simple.bat
+++ scripts/vol_up_simple.bat
@@ -1,3 +1,3 @@
 @echo off
 REM Windows volume up using PowerShell
-powershell -Command "$obj = New-Object -com WScript.Shell; for ($i = 0; $i -lt 3; $i++) { $obj.SendKeys([char]175); Start-Sleep -Milliseconds 50 }; Write-Host 'Volume increased'"
+start /min "" powershell -Command "$obj = New-Object -com WScript.Shell; for ($i = 0; $i -lt 3; $i++) { $obj.SendKeys([char]175); Start-Sleep -Milliseconds 50 }; Write-Host 'Volume increased'"
